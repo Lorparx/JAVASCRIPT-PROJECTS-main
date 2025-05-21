373ed112fdde2a6efde19d84f70618aa096deeef
Lorparx <laurarparks@gmail.com>
2025-05-21 10:28:14 -0700

    Javascript Project 2

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+function updateContent() {
+    // Assign values to two variables
+    let x = "Hello, World! ";
+    let y = " Welcome to JavaScript!";
+    let result3 = x + y; 
+    document.write(result3); // This will write the concatenated string to the document
+
+}
